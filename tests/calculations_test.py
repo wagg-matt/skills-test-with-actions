06983decc604c843d2e5bccccbfaca3650ca8f0f
Matt Wagg <wagg-matt@users.noreply.github.com>
2026-09-17 15:35:42 +0000

    added tests

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -34,6 +34,16 @@ def test_area_of_circle_zero_radius():
     assert result == 0
 
 
+def test_area_of_circle_negative_radius():
+    """Test with a negative radius to raise ValueError."""
+    # Arrange
+    radius = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        area_of_circle(radius)
+
+
 def test_get_nth_fibonacci_zero():
     """Test with n=0."""
     # Arrange
@@ -56,6 +66,16 @@ def test_get_nth_fibonacci_one():
 
     # Assert
     assert result == 1
+
+
+def test_get_nth_fibonacci_negative():
+    """Test with a negative number to raise ValueError."""
+    # Arrange
+    n = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(n)
 
 
 def test_get_nth_fibonacci_ten():
